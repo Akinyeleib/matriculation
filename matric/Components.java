6b8cd1e085832d3583409ff16663869a971b9754
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-07-14 14:44:24 +0100

    Components adjustments

--- matric/Components.java
+++ matric/Components.java
@@ -9,15 +9,12 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class Components {
+public interface Components {
+    
+    static Color fgColor = Color.YELLOW, bgColor = Color.BLACK;
+    static Font font = new Font("Ink Free", Font.ITALIC, 20);
 
-    Color fgColor = Color.YELLOW, bgColor = Color.BLACK;
-    Font font;
-    JTextField userNameField;
-    JPasswordField passwordField;
-    JButton submit;
-
-    private JButton createButton(String txt, ActionListener listener) {
+    private static JButton createButton(String txt, ActionListener listener) {
 
         JButton btn = new JButton(txt);
         btn.setForeground(fgColor);
@@ -29,7 +26,7 @@ public class Components {
         return btn;
     }
 
-    private JTextField createTextField() {
+    private static JTextField createTextField() {
         JTextField txtField = new JTextField(50);
         txtField.setFont(font);
         txtField.setForeground(fgColor);
@@ -39,7 +36,7 @@ public class Components {
         return txtField;
     }
 
-    private JPasswordField createPasswordField() {
+    private static JPasswordField createPasswordField() {
         JPasswordField txtField = new JPasswordField(50);
         txtField.setFont(font);
         txtField.setForeground(fgColor);
@@ -49,7 +46,7 @@ public class Components {
         return txtField;
     }
 
-    private JLabel createLabel(String txt) {
+    private static JLabel createLabel(String txt) {
         JLabel label = new JLabel(txt);
         label.setFont(font);
         label.setForeground(fgColor);
