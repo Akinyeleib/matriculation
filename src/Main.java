19fdf17c19d5113c36f190023639572ccf6a067c
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-07-15 21:15:05 +0100

    Launcher

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import matric.setup.Faculty;
+import matric.user.SignUp;
+
 public class Main {
 
     public static void main(String[] args) {
-
+//        new SignUp();
+        new Faculty();
     }
 
 }
